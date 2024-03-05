5b65ca3d9c5b7db9871a30a89a386c1e75bc9fc4
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2024-03-04 23:32:18 -0500

    added api

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,9 @@ function CookieStandHeader() {
   return (
     <header className="flex justify-between bg-green-500 text-black-100 p-3 items-center">
       <h1 className="font-semibold text-4xl font-sans mx-9">Cookie Stand Admin</h1>
+      <Link href='/overview' legacyBehavior>
+        <a className="bg-gray-100 text-gray-800 rounded-lg mr-4 pb-1 pl-1 pr-1 text-l float-right">Overview</a>
+      </Link>
     </header>
   );
 }
